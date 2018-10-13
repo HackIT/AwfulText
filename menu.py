62f72633ba6c37dd3769e235c56bd3504e283be3
0x44 <me@you>
2018-10-13 12:18:27 +0200

    refactorization.

--- menu.py
+++ menu.py
@@ -7,122 +7,205 @@ from dialog import About
 # File                View                              Tools                    Help
 #   | ---> NewFile      | ---> Show menubar               | ---> build             | ---> about
 #   | ---> OpenFile     | ---> Show statusbar             | ---> colorpicker
-#   | ---> SaveFile     | ---> Show sidebar
-#   | ---> SaveAs       | ---> Show console
-#   | ---> Quit         | ---> Show line numbers
-#                       | ---> Highlight current line
-#
+#   | ---> OpenFolder   | ---> Show sidebar
+#   | ---> AddFolder    | ---> Show console
+#   | ---> Save         | ---> Show line numbers
+#   | ---> SaveAs       | ---> Highlight current line
+#   | ---> Quit
+
 # TODO -
 # : open recent file
 
 class Menubar( gtk.MenuBar ):
     def __init__( self, gtkWindow ):
         super( Menubar, self ).__init__()
-        menuFile = gtk.MenuItem( "_File" )
-        menuFileItem = gtk.Menu()
-        menuFile.set_submenu( menuFileItem )
 
-        menuFileItem.append( self.menuPush( gtk.STOCK_NEW, "<Control>N", gtkWindow.buffer.newFile, gtkWindow ) )
-        menuFileItem.append( self.menuPush( gtk.STOCK_OPEN, "<Control>O", gtkWindow.buffer.openFile, gtkWindow ) )
-        
-        openFolder = gtk.MenuItem( "Open Folder" )
-        openFolder.connect( "activate", gtkWindow.filebrowser.openFolder )
-        menuFileItem.append( openFolder )
-
-        addFolder = gtk.MenuItem( "Add Folder" )
-        addFolder.connect( "activate", gtkWindow.filebrowser.addFolder )
-        menuFileItem.append( addFolder )
-
-        #menuFileItem.append( self.menuPush( gtk.STOCK_OPEN, "Open folder...", gtkWindow.filebrowser.openFolder, gtkWindow ) )
-        menuFileItem.append( self.menuPush( gtk.STOCK_SAVE, "<Control>S", gtkWindow.buffer.save, gtkWindow ) )
-        menuFileItem.append( self.menuPush( gtk.STOCK_SAVE_AS, "<Shift><Control>S", gtkWindow.buffer.save_as, gtkWindow ) )
-        menuFileItem.append( gtk.SeparatorMenuItem() )
-        menuFileItem.append( self.menuPush( gtk.STOCK_QUIT, "<Control>Q", gtkWindow.mainQuit, gtkWindow ) )
-
-        menuView = gtk.MenuItem( "_View" )
-        menuViewItem = gtk.Menu()
-        menuView.set_submenu( menuViewItem )
-
-        showMenubar = gtk.CheckMenuItem( "Show Menubar" )
-        showMenubar.set_active( config.window_menu )
-        showMenubar.connect( "activate", self.menubarView )
-        menuViewItem.append( showMenubar )
-
-        showStatusbar = gtk.CheckMenuItem( "Show Statusbar" )
-        showStatusbar.set_active( config.window_statusbar )
-        showStatusbar.connect( "activate", gtkWindow.statusbar.statusbarView )
-        menuViewItem.append( showStatusbar )
-        
-        menuViewItem.append( gtk.SeparatorMenuItem() )
-        
-        showDirBrowser = gtk.CheckMenuItem( "Show Sidebar" )
-        showDirBrowser.set_active( config.window_sidepane )
-        showDirBrowser.connect( "activate", gtkWindow.toggleFileBrowser )
-        menuViewItem.append( showDirBrowser )
-
-        showConsole = gtk.CheckMenuItem( "Show Console" )
-        showConsole.set_active( config.window_console )
-        showConsole.connect( "activate", gtkWindow.toggleConsole )
-        menuViewItem.append( showConsole )
-
-        menuViewItem.append( gtk.SeparatorMenuItem() )
-
-        showLineNumbers = gtk.CheckMenuItem( "Show line numbers" )
-        showLineNumbers.set_active( config.show_line_numbers )
-        showLineNumbers.connect( "activate", gtkWindow.view.toggleLineNumbers )
-        menuViewItem.append( showLineNumbers )
-
-        highlightCurrentLine = gtk.CheckMenuItem( "Highlight current line" )
-        highlightCurrentLine.set_active( config.highlight_current_line )
-        highlightCurrentLine.connect( "activate", gtkWindow.view.toggleHighlightCurrentLine )
-        menuViewItem.append( highlightCurrentLine )
-
-        menuTools = gtk.MenuItem( "_Tools" )
-        menuToolsItem = gtk.Menu()
-        menuTools.set_submenu( menuToolsItem )
-        menuToolsItem.append( self.menuPush( "Build", "<Control>B", exit, gtkWindow ) )
-        menuToolsItem.append( self.menuPush( "ColorPicker", "<Control>F", tools.ColorDial, gtkWindow ) )
-        #buildTool = gtk.MenuItem( "build" )
-        #buildTool.connect( "activate", exit )
-        #menuToolsItem.append( buildTool )
-
-        menuHelp = gtk.MenuItem( "_Help" )
-        menuHelpItem = gtk.Menu()
-        menuHelp.set_submenu( menuHelpItem )
-
-        aboutDial = gtk.MenuItem( "about" )
-        aboutDial.connect( "activate", About )
-        menuHelpItem.append( aboutDial )
-
-        self.append( menuFile )
-        self.append( menuView )
-        self.append( menuTools )
-        self.append( menuHelp )
-
-    def menuPush( self, strdata, binding, activate, gtkWindow ):
         accelGroup = gtk.AccelGroup()
         gtkWindow.add_accel_group( accelGroup )
-        menuItem = gtk.ImageMenuItem( strdata, accelGroup )
-        key, mod = gtk.accelerator_parse( binding )
-        menuItem.add_accelerator( "activate",
-            accelGroup, key, mod, gtk.ACCEL_VISIBLE
-        )
-        if activate:
-            menuItem.connect( "activate", activate )
-        return menuItem
 
-    def menuCreate(self, name):
-        menu = gtk.MenuItem(name)
-        subMenu = gtk.Menu()
-        menu.set_submenu(subMenu)
-        return menu, subMenu
+        # type:
+        # 0 == gtk.MenuItem
+        # 1 == gtk.ImageMenuItem
+        # 2 == gtk.CheckMenuItem
+        # 3 == gtk.SeparatorMenuItem()
+        menus = [ \
+            {
+            'menu_label':"_File",
+            'menu_items':[
+                {
+                    'type':1,
+                    'stockId':gtk.STOCK_NEW,
+                    'label':'New File',
+                    'binding':"<Control>N",
+                    'activate': gtkWindow.buffer.newFile
+                },
+                {
+                    'type':1,
+                    'stockId':gtk.STOCK_OPEN,
+                    'label':'Open File',
+                    'binding':"<Control>O",
+                    'activate': gtkWindow.buffer.openFile
+                },
+                {
+                    'type':1,
+                    'stockId':gtk.STOCK_OPEN,
+                    'label':'Open Folder',
+                    'binding':None,
+                    'activate': gtkWindow.filebrowser.openFolder
+                },
+                {
+                    'type':1,
+                    'stockId':gtk.STOCK_ADD,
+                    'label':'Add Folder',
+                    'binding':None,
+                    'activate': gtkWindow.filebrowser.addFolder
+                },
+                {
+                    'type':1,
+                    'stockId':gtk.STOCK_SAVE,
+                    'label':'Save',
+                    'binding':'<Control>S',
+                    'activate': gtkWindow.buffer.save
+                },
+                {
+                    'type':1,
+                    'stockId':gtk.STOCK_SAVE_AS,
+                    'label': 'Save As',
+                    'binding':'<Shift><Control>S',
+                    'activate': gtkWindow.buffer.saveAs
+                },
+                {'type':3 },
+                {
+                    'type':1,
+                    'stockId':gtk.STOCK_QUIT,
+                    'label':"Quit",
+                    'binding':"<Control>Q",
+                    'activate': gtkWindow.mainQuit
+                }
+            ]},
+            {
+            'menu_label':"_View",
+            'menu_items':[
+                {
+                    'type':2,
+                    'label':"Show Menubar",
+                    'is_active':config.window_menu,
+                    'activate':self.menubarView
+                },
+                {
+                    'type':2,
+                    'label':"Show Statusbar",
+                    'is_active':config.window_statusbar,
+                    'activate':gtkWindow.statusbar.statusbarView
+                },
+                {'type':3 },
+                {
+                    'type':2,
+                    'label':"Show Side pane",
+                    'is_active':config.window_sidepane ,
+                    'activate':gtkWindow.toggleFileBrowser
+                },
+                {
+                    'type':2,
+                    'label':"Show Console",
+                    'is_active':config.window_console ,
+                    'activate':gtkWindow.toggleConsole
+                },
+                {'type':3 },
+                {
+                    'type':2,
+                    'label':"Show line numbers",
+                    'is_active':config.show_line_numbers ,
+                    'activate':gtkWindow.view.toggleLineNumbers
+                },
+                {
+                    'type':2,
+                    'label':"Highlight current line",
+                    'is_active':config.highlight_current_line ,
+                    'activate':gtkWindow.view.toggleHighlightCurrentLine
+                }
+            ]},
+            {
+            'menu_label':"_Tools",
+            'menu_items':[
+                {
+                    'type':0,
+                    'label':'Build',
+                    'binding':'<Control>B',
+                    'activate': exit
+                },
+                {
+                    'type':0,
+                    'label':'ColorPicker',
+                    'binding':'<Control>F',
+                    'activate':tools.ColorDial
+                }
+            ]},
+            {
+            'menu_label':"_Help",
+            'menu_items':[
+                {
+                    'type':1,
+                    'stockId':gtk.STOCK_ABOUT,
+                    'label':"About",
+                    'binding':None,
+                    'activate':About
+                }
+            ]}
+        ]
+
+        for i in menus: # root menus
+            menu_label = gtk.MenuItem( i['menu_label'] )
+            menu_label_items = gtk.Menu()
+            menu_label.set_submenu( menu_label_items )
+            for n in i['menu_items']:
+                menu_item = None
+                if n['type'] == 3: # gtk.SeparatorMenuItem
+                    menu_label_items.append( gtk.SeparatorMenuItem() )
+                    continue
+
+                if n['type'] == 2: # gtk.CheckMenuItem
+                    menu_item = gtk.CheckMenuItem( n['label'] )
+                    if n['is_active']:
+                        menu_item.set_active( n['is_active'] )
+                    if n['activate']:
+                        menu_item.connect('activate', n['activate'])
+                    menu_label_items.append(menu_item)
+                    continue
+
+                if n['type'] == 1: # gtk.ImageMenuItem
+                    if n['stockId']:
+                        menu_item = gtk.ImageMenuItem( n['stockId'], accelGroup )
+                        menu_item.set_label(n['label'])
+                        if n['binding']:
+                            key, mod = gtk.accelerator_parse( n['binding'] )
+                            menu_item.add_accelerator( "activate",
+                                accelGroup, key, mod, gtk.ACCEL_VISIBLE
+                            )
+                        if n['activate']:
+                            menu_item.connect('activate', n['activate'])
+                    menu_label_items.append(menu_item)
+                    continue
+
+                if n['type'] == 0: # gtk.MenuItem
+                    menu_item = gtk.MenuItem( n['label'] )
+                    if n['binding']:
+                        key, mod = gtk.accelerator_parse( n['binding'] )
+                        menu_item.add_accelerator( "activate",
+                            accelGroup, key, mod, gtk.ACCEL_VISIBLE
+                        )
+                    if n['activate']:
+                        menu_item.connect('activate', n['activate'])
+                    menu_label_items.append(menu_item)
+                    continue
+            self.append(menu_label)
 
     def menubarView(self, checkMenuItem):
         if checkMenuItem.active:
             self.enable()
         else:
             self.disable()
-    
+
     def enable(self):
         self.show()
 
