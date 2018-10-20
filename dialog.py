40ac96936015716d414d4de4d0a897fec9a5234f
0x44 <me@you>
2018-10-20 18:41:31 +0200

    adding colordial to dialog.py finally.

--- dialog.py
+++ dialog.py
@@ -46,3 +46,19 @@ class OpenFolderDialog( gtk.FileChooserDialog ):
             (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL,
             gtk.STOCK_OPEN, gtk.RESPONSE_OK)
         )
+
+class ColorDial( gtk.ColorSelectionDialog ):
+    def __init__( self, arg ):
+        super( ColorDial, self ).__init__("Select color")
+        self.set_position(gtk.WIN_POS_CENTER)
+        response = self.run()
+        if response == gtk.RESPONSE_OK:
+            colorsel = self.colorsel
+            m = colorsel.get_current_color()
+            print m.red, m.green, m.blue, colorsel.get_current_color().to_string(), self.get_color_selection()
+        # print response
+        #if response == gtk.RESPONSE_OK:
+        #    colorsel = colordial.colorsel
+        #    color = colorsel.get_current_color()
+        #    self.label.modify_fg(gtk.STATE_NORMAL, color)
+        self.destroy()
