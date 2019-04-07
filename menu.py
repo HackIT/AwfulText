fa05c9164c080a26a3af89934edbf257214c5039
[Awaxx] <boxx1@free.fr>
2019-04-07 18:27:26 +0200

    Give a mean to menu types.

--- menu.py
+++ menu.py
@@ -5,6 +5,13 @@ import gtk, config
 
 from dialog import About, ColorDial
 
+(
+ MENU_ITEM,
+ IMAGE_MENU_ITEM,
+ CHECK_MENU_ITEM,
+ MENU_SEPARATOR
+) = range (4)
+
 # File                View                              Tools                    Help
 #   | ---> NewFile      | ---> Show menubar               | ---> build             | ---> about
 #   | ---> OpenFile     | ---> Show statusbar             | ---> colorpicker
@@ -31,57 +38,57 @@ class Menubar( gtk.MenuBar ):
             'menu_label':"_File",
             'menu_items':[
                 {
-                    'type':1,
+                    'type':IMAGE_MENU_ITEM,
                     'stockId':gtk.STOCK_NEW,
                     'label':'New File',
                     'binding':"<Control>N",
                     'activate': gtkWindow.newFile
                 },
                 {
-                    'type':1,
+                    'type':IMAGE_MENU_ITEM,
                     'stockId':gtk.STOCK_OPEN,
                     'label':'Open File',
                     'binding':"<Control>O",
                     'activate': gtkWindow.openFile
                 },
                 {
-                    'type':1,
+                    'type':IMAGE_MENU_ITEM,
                     'stockId':gtk.STOCK_OPEN,
                     'label':'Open Folder',
                     'binding':None,
                     'activate': gtkWindow.openFolder
                 },
                 {
-                    'type':1,
+                    'type':IMAGE_MENU_ITEM,
                     'stockId':gtk.STOCK_ADD,
                     'label':'Add Folder',
                     'binding':None,
                     'activate': gtkWindow.addFolder
                 },
                 {
-                    'type':1,
+                    'type':IMAGE_MENU_ITEM,
                     'stockId':gtk.STOCK_SAVE,
                     'label':'Save',
                     'binding':'<Control>S',
                     'activate': gtkWindow.save
                 },
                 {
-                    'type':1,
+                    'type':IMAGE_MENU_ITEM,
                     'stockId':gtk.STOCK_SAVE_AS,
                     'label': 'Save As',
                     'binding':'<Shift><Control>S',
                     'activate': gtkWindow.saveAs
                 },
                 {
-                    'type':1,
+                    'type':IMAGE_MENU_ITEM,
                     'stockId':gtk.STOCK_CLOSE,
                     'label': 'Close file',
                     'binding':'<Control>W',
                     'activate': gtkWindow.close
                 },
-                {'type':3 },
+                {'type':MENU_SEPARATOR },
                 {
-                    'type':1,
+                    'type':IMAGE_MENU_ITEM,
                     'stockId':gtk.STOCK_QUIT,
                     'label':"Quit",
                     'binding':"<Control>Q",
@@ -92,39 +99,39 @@ class Menubar( gtk.MenuBar ):
             'menu_label':"_View",
             'menu_items':[
                 {
-                    'type':2,
+                    'type':CHECK_MENU_ITEM,
                     'label':"Show Menubar",
                     'is_active':config.window_menu,
                     'activate':self.menubarView
                 },
                 {
-                    'type':2,
+                    'type':CHECK_MENU_ITEM,
                     'label':"Show Statusbar",
                     'is_active':config.window_statusbar,
                     'activate':gtkWindow.statusbar.statusbarView
                 },
-                {'type':3 },
+                {'type':MENU_SEPARATOR },
                 {
-                    'type':2,
+                    'type':CHECK_MENU_ITEM,
                     'label':"Show Side pane",
                     'is_active':config.window_sidepane ,
                     'activate':gtkWindow.toggleFolderTree
                 },
                 {
-                    'type':2,
+                    'type':CHECK_MENU_ITEM,
                     'label':"Show Console",
                     'is_active':config.window_console ,
                     'activate':gtkWindow.toggleConsole
                 },
-                {'type':3 },
+                {'type':MENU_SEPARATOR },
                 {
-                    'type':2,
+                    'type':CHECK_MENU_ITEM,
                     'label':"Show line numbers",
                     'is_active':config.show_line_numbers ,
                     'activate':gtkWindow.view.toggleLineNumbers
                 },
                 {
-                    'type':2,
+                    'type':CHECK_MENU_ITEM,
                     'label':"Highlight current line",
                     'is_active':config.highlight_current_line ,
                     'activate':gtkWindow.view.toggleHighlightCurrentLine
@@ -134,13 +141,13 @@ class Menubar( gtk.MenuBar ):
             'menu_label':"_Tools",
             'menu_items':[
                 {
-                    'type':0,
+                    'type':MENU_ITEM,
                     'label':'Build',
                     'binding':'<Control>B',
                     'activate': exit
                 },
                 {
-                    'type':0,
+                    'type':MENU_ITEM,
                     'label':'ColorPicker',
                     'binding':'<Control>F',
                     'activate':ColorDial
@@ -150,7 +157,7 @@ class Menubar( gtk.MenuBar ):
             'menu_label':"_Help",
             'menu_items':[
                 {
-                    'type':1,
+                    'type':IMAGE_MENU_ITEM,
                     'stockId':gtk.STOCK_ABOUT,
                     'label':"About",
                     'binding':None,
@@ -165,11 +172,11 @@ class Menubar( gtk.MenuBar ):
             menu_label.set_submenu( menu_label_items )
             for n in i['menu_items']:
                 menu_item = None
-                if n['type'] == 3: # gtk.SeparatorMenuItem
+                if n['type'] == MENU_SEPARATOR: # gtk.SeparatorMenuItem
                     menu_label_items.append( gtk.SeparatorMenuItem() )
                     continue
 
-                if n['type'] == 2: # gtk.CheckMenuItem
+                if n['type'] == CHECK_MENU_ITEM: # gtk.CheckMenuItem
                     menu_item = gtk.CheckMenuItem( n['label'] )
                     if n['is_active']:
                         menu_item.set_active( n['is_active'] )
@@ -178,7 +185,7 @@ class Menubar( gtk.MenuBar ):
                     menu_label_items.append(menu_item)
                     continue
 
-                if n['type'] == 1: # gtk.ImageMenuItem
+                if n['type'] == IMAGE_MENU_ITEM: # gtk.ImageMenuItem
                     if n['stockId']:
                         menu_item = gtk.ImageMenuItem( n['stockId'] )
                         menu_item.set_label(n['label'])
@@ -193,7 +200,7 @@ class Menubar( gtk.MenuBar ):
                     menu_label_items.append(menu_item)
                     continue
 
-                if n['type'] == 0: # gtk.MenuItem
+                if n['type'] == MENU_ITEM: # gtk.MenuItem
                     menu_item = gtk.MenuItem( n['label'] )
                     if n['binding']:
                         key, mod = gtk.accelerator_parse( n['binding'] )
